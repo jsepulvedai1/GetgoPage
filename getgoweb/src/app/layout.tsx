4d183ac67b155abbb625e12d5c36e8e650597339
Javier Sepulveda <javiersepulveda@MacBook-Air-de-Javier.local>
2025-02-01 20:25:20 -0300

    commit

--- getgoweb/src/app/layout.tsx
+++ getgoweb/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Montserrat } from 'next/font/google';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
