7e846343e4296132fd96dbe2176f7b24746176bb
Castieell <martinmoreno@live.cl>
2025-02-19 16:11:18 -0300

    ultimos cambios

--- getgoweb/src/app/page.tsx
+++ getgoweb/src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { Montserrat } from "next/font/google";
 import Navbar from "./components/navbar";
 import { motion } from "framer-motion";
-import SocialSidebar, { }  from "./components/social"
+import SocialSidebar, { } from "./components/social"
 import StoreButtons from "./components/store";
 
 
@@ -22,27 +22,20 @@ export default function Home() {
         <Navbar />
       </header>
       <section
-       className="{`${montserrat.className}relative w-full h-[80vh] md:h-[70vh] flex items-center justify-start px-6 md:px-12 bg-cover bg-center"
+        className="{`${montserrat.className}relative w-full h-[80vh] md:h-[70vh] flex items-center justify-start px-6 md:px-12 bg-cover bg-center"
         style={{ backgroundImage: "url('/images/im.png')" }}
       >
         <div className="w-full md:w-1/2 text-left justify-center pt-32 md:pt-48 ml-0">
           <motion.h2
-            className="text-3xl md:text-5xl font-bold text-white leading-tight uppercase mb-6"
+            className="text-3xl md:text-4xl font-bold text-white leading-tight uppercase mb-6"
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
           >
-            Regístrate en minutos <br /> gana al instante
+            Regístrate en minutos <br />
+            <span className="text-4xl md:text-6xl text-white">gana al instante</span>
           </motion.h2>
 
-          <motion.p
-            className="text-lg md:text-xl text-white mb-8"
-            initial={{ opacity: 0, x: -50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, delay: 0.5 }}
-          >
-            Maneja con GetGo y convierte tu tiempo libre en dinero
-          </motion.p>
         </div>
       </section>
 
@@ -109,6 +102,19 @@ export default function Home() {
           ))}
         </motion.div>
       </motion.section>
+
+      <div className="bg-[#db2392] text-white py-8 px-6 md:px-12  text-center md:text-center">
+        <h2 className="text-2xl md:text-3xl font-bold mb-4">
+          Conoce nuestro sistema de referidos
+        </h2>
+        <br></br>
+        <a
+          href="/pagina-referidos"  // Aquí pones la ruta de la página a la que deseas redirigir
+          className="inline-block bg-white text-[#db2392] py-3 px-7 rounded-lg text-lg font-semibold hover:bg-gray-200 transition"
+        >
+          CONOCE NUESTRO SISTEMA
+        </a>
+      </div>
       {/* QR Section */}
       <section className="px-6 md:px-12 py-16 flex flex-col md:flex-row items-center gap-12">
         <div className="w-full md:w-1/2 text-center md:text-left">
@@ -120,19 +126,11 @@ export default function Home() {
             viaje empieza aqui!
           </p>
           <StoreButtons />
-          <div className="hidden md:flex justify-center items-center mt-6 w-full">
-            <Image
-              src="/images/codigoQR.png"
-              alt="QR Code"
-              width={120}
-              height={120}
-              className="w-32 md:w-40 h-auto"
-            />
-          </div>
+
         </div>
         <div className="w-full md:w-1/2 flex justify-center">
           <Image
-            src="/images/phone-app.png"
+            src="/images/codigoQR.png"
             alt="App en Teléfono"
             width={400}
             height={400}
@@ -141,45 +139,41 @@ export default function Home() {
         </div>
       </section>
       {/* Footer */}
-      <footer className="bg-[#000080] text-white py-8 px-6 md:px-12 rounded-t-3xl text-center md:text-left">
-        <div className="flex flex-col md:flex-row justify-between">
-          <div className="mb-6 md:mb-0">
-            <ul className="space-y-2 text-lg md:text-xl">
-              <li>LA EMPRESA</li>
-              <li>LEGAL</li>
-              <li>LA APP</li>
-            </ul>
-          </div>
-          <div>
-            <div className="flex justify-center md:justify-end gap-4 mb-4">
-              {["lin", "insta", "f", "x"].map((social, index) => (
-                <a
-                  key={index}
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Image
-                    src={`/images/${social}.png`}
-                    alt={social}
-                    width={30}
-                    height={30}
-                    className="w-6 h-6 cursor-pointer"
-                  />
-                </a>
-              ))}
-            </div>
-            <p className="text-lg">
-              Oficina Central Calle Paralelepípedo 9999, Santiago
-            </p>
-            <p className="text-lg">contacto@getgo.cl | (+56) 2 7575 2500</p>
-            <p className="text-sm mt-2">
-              Todos los derechos reservados GetGo® 2024
-            </p>
+      <footer className="bg-[#000080] text-white py-8 px-6 md:px-12 rounded-t-3xl text-center">
+        <div className="mb-6">
+          <ul className="flex justify-center space-x-6 text-lg md:text-xl">
+            <li>LA EMPRESA</li>
+            <li>LEGAL</li>
+            <li>LA APP</li>
+          </ul>
+        </div>
+
+        <div className="mb-4">
+          <div className="flex justify-center gap-6">
+            {["lin", "insta", "f", "x"].map((social, index) => (
+              <a
+                key={index}
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={`/images/${social}.png`}
+                  alt={social}
+                  width={30}
+                  height={30}
+                  className="w-6 h-6 cursor-pointer"
+                />
+              </a>
+            ))}
           </div>
         </div>
+
+        <p className="text-sm mt-2">
+          Todos los derechos reservados GetGo® 2024
+        </p>
       </footer>
-       <div className={`${montserrat.className}`}></div>
+      <div className={`${montserrat.className}`}></div>
     </div>
   );
 }
